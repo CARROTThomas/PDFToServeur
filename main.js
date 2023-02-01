d178d5833061d4c055b61b0ca5908ff9ca255b3f
CARROTThomas <thomas.carrot38240@gmail.com>
2023-02-01 16:13:15 +0100

    test

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-
-let stringC = "string1"
-
+/*
 const formDataImg = new FormData();
 const formDataPdf = new FormData();
 
 imgLocal = new Image()
 imgLocal.src = "img/trac.jpg"
 //console.log(imgLocal.src)
-
 pdfLocal = new Image()
 pdfLocal.src = "pdf/truc.pdf"
+
 
 baseUrl = "https://ascii-art.fr/"
 async function pdfToServeur(stringC) {
@@ -17,8 +15,9 @@ async function pdfToServeur(stringC) {
     let urlImg = `${baseUrl}uploadimage/${stringC}` // randomString
     let urlPdf = `${baseUrl}uploadpdf/` //id
 
-    formDataImg.append('asciicapture',imgLocal.files[0], imgLocal.src) // value = bloblo.jpg  /!\ taille /!\
-    formDataImg.append('asciicapture',new Blob(imgLocal, {image/png}), imgLocal.src) // value = bloblo.jpg  /!\ taille /!\
+    // value = bloblo.jpg  /!\ taille /!\
+    //formDataImg.append('asciicapture',imgLocal.files[0], imgLocal.src)chat
+    //formDataImg.append('asciicapture', , imgLocal.src) // value = bloblo.jpg  /!\ taille /!\
 
     let fetchParamsImg = {
         method: 'POST',
@@ -46,4 +45,72 @@ async function pdfToServeur(stringC) {
                 })
         })
 }
+
+ */
+
+
+
+
+
+
+
+baseUrl = "https://ascii-art.fr/"
+const formDataImg = new FormData();
+const formDataPdf = new FormData();
+
+const btnStart = document.querySelector('.btnStart')
+btnStart.addEventListener('click', ()=>{
+    const fileInput = document.querySelector(".input")
+    const fileImg = fileInput.files[0];
+    const inputPdf = document.querySelector(".inputPdf")
+    const filePdf = inputPdf.files[0];
+    getImage(fileImg, filePdf);
+})
+
+async function getImage(fileImg, filePdf) {
+    // Assume the fileImg is stored in the variable `fileImg`
+    let urlImg = `${baseUrl}uploadimage/stringRandomA1` // randomString
+    let urlPdf = `${baseUrl}uploadpdf/` //id
+    formDataImg.append("asciicapture", fileImg);
+
+    console.log('test')
+    console.log(fileImg)
+
+    let fetchParamsImg = {
+        method: 'POST',
+        redirect : 'follow',
+        body : formDataImg
+    }
+
+    await fetch(urlImg, fetchParamsImg)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            console.log(data.id);
+
+            // 2 fetch
+            formDataPdf.append('pdf', filePdf) // value = truc.pdf
+            urlPdf +=data.id
+            let fetchParamsPdf = {
+                method: 'POST',
+                redirect : 'follow',
+                body: formDataPdf
+            }
+            fetch(urlPdf, fetchParamsPdf)
+                .then(response=>response.json())
+                .then(result=>{
+                    console.log('test')
+                    console.log(result)
+                })
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+
+
+
+
+
 
